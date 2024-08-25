595d49c3ec59f2bccd96c97b1adabffb82d91647
jstella <jstella@almirall.com>
2024-08-25 19:48:50 +0200

    header: css structure

--- src/app/Components/Header/header.tsx
+++ src/app/Components/Header/header.tsx
@@ -2,6 +2,7 @@
 
 import React, { useState } from 'react';
 import './header.css';
+import Image from 'next/image';
 
 const Header: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -14,14 +15,21 @@ const Header: React.FC = () => {
     <>
       <header className=''>
         <nav className={`${isOpen ? 'open' : ''}`}>
-          <div className='logo'></div>
+          <a className='logo' href='/'>
+            <Image
+              height={30}
+              width={30}
+              alt=''
+              src={''}
+            />
+          </a>
           <ul>
             <li><a href="#home">Home</a></li>
             <li><a href="#about">About</a></li>
             <li><a href="#contact">Contact</a></li>
             <li>
               <p>Websites</p>
-              <ul>
+              <ul className='websites_submenu'>
                 <li><a href="/retail">Retail</a></li>
                 <li><a href="/restaurant">Restaurant</a></li>
                 <li><a href="/real-state">Real State</a></li>
@@ -29,12 +37,12 @@ const Header: React.FC = () => {
             </li>
           </ul>
           <div
-            className='menu'
+            className='menu_mobile'
             onClick={(e) => toggleMenu()}
             aria-label="Toggle menu"
             aria-expanded={isOpen}
           >
-            <span className='menu_icon'></span>
+            <span className='menu_mobile_icon'></span>
           </div>
         </nav>
       </header>
