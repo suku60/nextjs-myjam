bcae0339b45b721483de710e499dd1864ecbe044
jstella <jstella@almirall.com>
2024-08-25 19:43:57 +0200

    header: htmlv.2.0

--- src/app/Components/Header/header.tsx
+++ src/app/Components/Header/header.tsx
@@ -11,24 +11,34 @@ const Header: React.FC = () => {
   };
 
   return (
-    <header className=''>
-      <div className='logo'></div>
-      <nav className={`${isOpen ? 'open' : ''}`}>
-        <ul>
-          <li><a href="#home">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#contact">Contact</a></li>
-        </ul>
-      </nav>
-      <button
-        className='menu'
-        onClick={(e) => toggleMenu()}
-        aria-label="Toggle menu"
-        aria-expanded={isOpen}
-      >
-        <span className='menu_icon'></span>
-      </button>
-    </header>
+    <>
+      <header className=''>
+        <nav className={`${isOpen ? 'open' : ''}`}>
+          <div className='logo'></div>
+          <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li>
+              <p>Websites</p>
+              <ul>
+                <li><a href="/retail">Retail</a></li>
+                <li><a href="/restaurant">Restaurant</a></li>
+                <li><a href="/real-state">Real State</a></li>
+              </ul>
+            </li>
+          </ul>
+          <div
+            className='menu'
+            onClick={(e) => toggleMenu()}
+            aria-label="Toggle menu"
+            aria-expanded={isOpen}
+          >
+            <span className='menu_icon'></span>
+          </div>
+        </nav>
+      </header>
+    </>
   );
 };
 
