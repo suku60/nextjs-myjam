e23a6d8a050de2823949cf69b070962aed0e071e
jstella <jstella@almirall.com>
2024-08-25 20:56:24 +0200

    fix layout module

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "./components/Header/header";
+import Header from "./Components/Header/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
