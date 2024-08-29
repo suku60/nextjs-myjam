5b3fd453c0782f9265d909f6566d9de9f0ba74f6
jstella <jstella@almirall.com>
2024-08-29 20:54:15 +0200

    added fonts

--- src/app/Components/Header/header.tsx
+++ src/app/Components/Header/header.tsx
@@ -6,10 +6,11 @@ import Image from 'next/image';
 import { useIsMobile } from '@/app/hooks/useIsMobile';
 
 const Header: React.FC = () => {
-  const isMobile1024 = useIsMobile(1024)
+  const isMobile1024 = useIsMobile(1024);
 
   const [isOpen, setIsOpen] = useState(false);
   const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+  const [isMenuHovered, setIsMenuHovered] = useState(false);
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
@@ -31,7 +32,10 @@ const Header: React.FC = () => {
               src={'/assets/logo.jpg'}
             />
           </a>
-          <ul>
+          <ul
+            onMouseOver={() => { setIsMenuHovered(true); }}
+            onMouseLeave={() => { setIsMenuHovered(false); }}
+            className={`${isMenuHovered ? 'active' : ''}`}>
             <li><a href="#home">Home</a></li>
             <li><a href="#about">About</a></li>
             <li><a href="#contact">Contact</a></li>
@@ -41,10 +45,11 @@ const Header: React.FC = () => {
                 aria-label="Toggle Websites submenu"
                 aria-expanded={isOpen}
               >Websites</p>
-              <ul className={`websites_submenu ${isSubmenuOpen ? 'active' : null}`}>
+              <ul className={`websites_submenu ${isSubmenuOpen ? 'active' : ''}`}>
                 <li><a href="/retail">Retail</a></li>
                 <li><a href="/restaurant">Restaurant</a></li>
-                <li><a href="/real-state">Real&nbsp;State</a></li>
+                <li><a href="/real-state">Real State</a></li>
+                <li><a href="/social-media">Social Media</a></li>
               </ul>
             </li>
           </ul>
